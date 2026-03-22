d338a1e77c42aae0e9fdcaaa026cf204d94bacdd
suraj <sharadadde@gmail.com>
2026-03-22 14:05:54 +0530

    UC11

---
+++ src/main/java/org/apps/quantitymeasurement/IMeasurableUC11.java
@@ -0,0 +1,8 @@
+package org.apps.quantitymeasurement;
+
+public interface IMeasurableUC11 {
+    double getConversionFactor();
+    double convertToBaseUnit(double value);
+    double convertFromBaseUnit(double baseValue);
+    String getUnitName();
+}
